795f35344ad1c39585263e11fdcc6a79b20e602b
JD Briones <jdbriones22@gmail.com>
2025-10-14 13:52:06 +1300

    App Setup

---
+++ app/index.d.ts
@@ -0,0 +1,45 @@
+declare interface BaseUser {
+    id: string;
+    name: string;
+    email: string;
+    dateJoined: string;
+    imageUrl: string;
+}
+
+declare interface UserData extends BaseUser {
+    itineraryCreated: number | string;
+    status: "user" | "admin";
+}
+
+declare type User = BaseUser;
+
+declare interface Trip {
+    id: string;
+    name: string;
+    country: string;
+    startDate: string;
+    endDate: string;
+    currencies: string[];
+    members: string[];
+}
+
+declare interface Expense {
+    id: string;
+    name: string;
+    amount: number;
+    category: string;
+    currency: string;
+    paidBy: string;
+    isAll: boolean;
+    sharedWith: string[];
+}
+
+declare interface Payout {
+    id: string;
+    owingUser: string;
+    owedUser: string;
+    amount: number;
+    item: string;
+    hasPaid: boolean;
+}
+
